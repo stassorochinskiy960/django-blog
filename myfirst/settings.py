d52452121641dcaeefab923ac2188fc3e751f364
Stas S <stassorochinskiy960@gmail.com>
2022-06-25 21:25:32 +0300

    3 commit

--- myfirst/settings.py
+++ myfirst/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qj=ty2sfkkuky!h001u+kqv@$9r6y(00ept+v14hoxq3-icg&w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['arcane-atoll-67379.herokuapp.com']
 
 
 # Application definition
